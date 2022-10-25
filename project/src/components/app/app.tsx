2015171552eb640ff9834b9b633bdfe1b6a50d81
AleksandrKubik <likisoko@gmail.com>
2022-10-25 10:46:20 +0300

    Fix Bug

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,32 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
-}
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {MainPage} from '../../pages/main-page/main-page';
+import {LoginPage} from '../../pages/login-page/login-page';
+import {PropertyPage} from '../../pages/property-page/property-page';
 
+type ApartmentCardType = {
+  apartmentData:{
+  mark?: string;
+  url: string;
+  imgUrl: string;
+  price: number;
+  ratingPercent: number;
+  description: string;
+  type: string;
+  }[];
+};
+
+
+function App({apartmentData}: ApartmentCardType): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route index element={<MainPage apartmentCardList = {apartmentData}/>}/>
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/property' element={<PropertyPage />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default App;
---
+++ project/src/types/apatmentType/apartmentType.tsx
@@ -0,0 +1,9 @@
+export type ApartmentCardType = {
+  mark?: string;
+  url: string;
+  imgUrl: string;
+  price: number;
+  ratingPercent: number;
+  description: string;
+  type: string;
+};
